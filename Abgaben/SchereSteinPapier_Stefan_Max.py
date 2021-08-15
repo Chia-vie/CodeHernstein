efc69f5f923580fb4428b20993305326b7c223b7
LennoxBrot <88963787+LennoxBrot@users.noreply.github.com>
2021-08-15 15:38:15 +0200

    Add files via upload

---
+++ Abgaben/SchereSteinPapier_Stefan_Max.py
@@ -0,0 +1,9 @@
+import random
+ranzahl = random.randint(0,2)
+moeglichkeiten = ['SCHERE', 'STEIN', 'PAPIER']
+ranzahl = int(ranzahl)
+computerwahl = moeglichkeiten[ranzahl]
+
+userwahl = input(f'Wähle Schere, Stein oder Papier!')
+
+
