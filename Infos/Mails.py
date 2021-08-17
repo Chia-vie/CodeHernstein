80f1dcc066142e5d57e8952f1b58511d5a9d27eb
Chia-vie <christine.ackerl@univie.ac.at>
2021-08-17 09:19:46 +0200

    sorted Abgaben

---
+++ Infos/Mails.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+import getpass
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+# ACHTUNG: Vorher Einstellungen ändern: Run > Edit Configurations > Emulate terminal in output console > Apply
+password = getpass.getpass('Please enter your password: ')
+sender_email = "sender.fuer.alle@gmail.com"
+receiver_email = "hernsteincoders@gmail.com"
+message = """\
+Subject: Hi there!
+
+Greetings."""
+
+# Send email here
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login("ciablue.test@gmail.com", password)
+    server.sendmail(sender_email, receiver_email, message)
+
