22e857fda9ede10414bf5423f99232d0bad80c01
Finn <finnomon@gmail.com>
2021-08-18 18:31:31 +0200

    Hab die Datei geändert4

--- Abgaben/Clicker_Game/Finn-App.py
+++ Abgaben/Clicker_Game/Finn-App.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 app = tk.Tk()
 app.geometry("1920x1080")
-app.title("Clicker_Game")
+app.title("Clicker Game")
+
 #def knopfgedrückt(text):
     #global out
     # die verschiedenen outputs einer Variable zuordnen
@@ -12,7 +13,7 @@ app.title("Clicker_Game")
 #beschreibung = tk.Label(app, text='Willkommen! Hier kannst du einen Knopf drücken!', font=('Helvetica', 16, 'bold'),
                         #bg='pale turquoise', fg='black', width = 40, height=2)
 
-header = tk.Label(app,text='Clicker_Game',
+header = tk.Label(app,text='Clicker Game',
                         font=('Gothic',24, 'bold'), bg='gray1', fg='red2', width = 40, height=2)
 header.pack()
 app.config(bg='gray1')
@@ -39,13 +40,31 @@ def onClick(event=None):
     counter.set(counter.get() + 1)
 
 tk.Label(app, textvariable=counter, width=200, height=5, fg="gray100", bg = "gray1").pack()
-tk.Button(app, text="Click for Point", command=onClick, fg="green2", activebackground = "gray1", bg = "gray1", width=10000, height=10000).pack()
+tk.Button(app, text="Click for Point", command=onClick, activeforeground="green2", fg="green2", activebackground = "gray1", bg = "gray1", width=10000, height=10000).pack()
 
 
 
 
+class Fullscreen_Example:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.attributes('-fullscreen', True)
+        self.fullScreenState = False
+        self.window.bind("<F11>", self.toggleFullScreen)
+        self.window.bind("<Escape>", self.quitFullScreen)
 
+        self.window.mainloop()
 
+    def toggleFullScreen(self, event):
+        self.fullScreenState = not self.fullScreenState
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
+    def quitFullScreen(self, event):
+        self.fullScreenState = False
+        self.window.attributes("-fullscreen", self.fullScreenState)
+
+if __name__ == '__main__':
+    app = Fullscreen_Example()
 
 
 
