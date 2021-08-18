4fa27917f68ebd47a963578f4705cab277303b72
Chia-vie <christine.ackerl@univie.ac.at>
2021-08-18 10:15:46 +0200

    geheim

--- Infos/Mails.py
+++ Infos/Mails.py
@@ -17,6 +17,6 @@ Greetings."""
 # Create a secure SSL context
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("ciablue.test@gmail.com", password)
+    server.login("sender.fuer.alle@gmail.com", password)
     server.sendmail(sender_email, receiver_email, message)
 
