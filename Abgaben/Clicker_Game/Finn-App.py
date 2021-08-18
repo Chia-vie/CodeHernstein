7290bdc8617104fc68cc606991aaa9019d051feb
Finn <finnomon@gmail.com>
2021-08-18 18:03:04 +0200

    Hab die Datei geändert

---
+++ Abgaben/Clicker_Game/Finn-App.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+app = tk.Tk()
+app.geometry("1920x1080")
+app.title("Clicker_Game")
+#def knopfgedrückt(text):
+    #global out
+    # die verschiedenen outputs einer Variable zuordnen
+    #out.set
+    #app.update_idletasks()
+
+
+#beschreibung = tk.Label(app, text='Willkommen! Hier kannst du einen Knopf drücken!', font=('Helvetica', 16, 'bold'),
+                        #bg='pale turquoise', fg='black', width = 40, height=2)
+
+#header = tk.Label(app,text='Clicker_Game',
+                        #font=('Gothic',24, 'bold'), bg='gray1', fg='red2', width = 40, height=2)
+#header.pack()
+app.config(bg='gray1')
+
+
+#out = tk.StringVar()
+#out.set('Drück den Knopf!')
+
+#rahmen = tk.Frame(app, bg='purple')
+#rahmen.pack(side='top', padx='10', pady='30')
+#knopf = tk.Button(app, text='0', font=('Gothic',20, 'bold'),
+                  #fg='blue4', width=20, height=5, command=lambda: knopfgedrückt('Chrisi'))
+
+
+
+#knopf.pack()
+
+
+
+
+counter = tk.IntVar()
+
+def onClick(event=None):
+    counter.set(counter.get() + 1)
+
+tk.Label(app, textvariable=counter, width=200, height=5, fg="gray100", bg = "gray1").pack()
+tk.Button(app, text="", command=onClick, fg="gray1", bg = "gray1", width=200, height=50).pack()
+
+app.mainloop()
+
+
+
+
+
+
+
+
+app.mainloop()
