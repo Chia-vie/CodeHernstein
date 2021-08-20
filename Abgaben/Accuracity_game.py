1ab619c1745c4451e98233a85bf0600443e893ea
Finn <finnomon@gmail.com>
2021-08-20 11:43:33 +0200

    Accuracity game Click Fertig

--- Abgaben/Accuracity_game.py
+++ Abgaben/Accuracity_game.py
@@ -1,56 +1,21 @@
 import tkinter as tk
+import random
 fenster = tk.Tk()
-fenster.config(bg = 'royalblue')
+fenster.config(bg = 'gray100')
 fenster.title('Dynamische Objekte in tkinter')
-
-
-
-
-
-
-fenster.geometry("1920x1080")
 feld = tk.Canvas(fenster, width=1920, height=1080, bd=0, bg='pale turquoise')
-feld.delete("all")
+feld.pack()
 
+counter = tk.IntVar()
 
+def clicked(*args):
+    counter.set(counter.get() + 1)
+    feld.delete("all")
 
-
-
-
-
-
-
-
-
+playbutton = feld.create_rectangle(1000, 500, 50, 1000, fill="red",tags="playbutton")
+Zaehler=tk.Label(fenster, font=('Gothic', 24, 'bold'), textvariable=counter, width=10, height=5, fg="gray100", bg="gray1")
+Zaehler.place(x=0, y=0)
+feld.tag_bind("playbutton","<Button-1>",clicked)
 
 
 fenster.mainloop()
-=======
-import tkinter as tk
-fenster = tk.Tk()
-fenster.config(bg = 'royalblue')
-fenster.title('Dynamische Objekte in tkinter')
-
-
-
-
-
-
-fenster.geometry("1920x1080")
-feld = tk.Canvas(fenster, width=1920, height=1080, bd=0, bg='pale turquoise')
-feld.delete("all")
-
-
-
-
-
-
-
-
-
-
-
-
-
-fenster.mainloop()
->>>>>>> origin/Finn_und_Niemand
