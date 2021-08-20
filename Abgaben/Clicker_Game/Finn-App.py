18451fd12bc08789ab817ec0ad50585e4cd8ef94
Finn <finnomon@gmail.com>
2021-08-20 11:08:44 +0200

    Accuracity game

--- Abgaben/Clicker_Game/Finn-App.py
+++ Abgaben/Clicker_Game/Finn-App.py
@@ -34,25 +34,36 @@ app.config(bg='gray1')
 # knopf.pack()
 
 counter = tk.IntVar()
-
+Generator1 = False
 
 def onclick():
     counter.set(counter.get() + 1)
+def whenclick():
+    global Generator1
+    Generator1 = True
 
 # def do(event):
 # print("Button Clicked!")
 # counter.set(counter.get() + 1)
 # ...
-
+if Generator1 == True:
+    while True:
+        time.sleep(10)
+        counter.set(counter.get() + 1)
 
 img = ImageTk.PhotoImage(Image.open("Unbenannt.png"))
 # tk.Button = Label(app, image = img, activeforeground="green2", fg="green2", activebackground="gray1", bg="gray1", width=100, height=50).pack()
 
 # tk.Button.bind('<Button-1>', do)
 
-tk.Label(app, font=('Gothic', 24, 'bold'), text="Hundefutter:", bg="gray1", fg="gray100", width=100).pack()
-tk.Label(app, font=('Gothic', 24, 'bold'), textvariable=counter, width=200, height=5, fg="gray100", bg="gray1").pack()
-tk.Button(app, image=img, command=onclick, activeforeground="green2", fg="green2", activebackground="gray1", bg="gray1", width=590, height=700).pack()
+button1 = tk.Label(app, font=('Gothic', 24, 'bold'), text="Hundefutter:", bg="gray1", fg="gray100", width=100)
+Zaehler=tk.Label(app, font=('Gothic', 24, 'bold'), textvariable=counter, width=10, height=5, fg="gray100", bg="gray1").pack()
+Clicks_dazu=tk.Button(app, image=img, command=onclick, activeforeground="green2", fg="green2", activebackground="gray1", bg="gray1", width=590, height=700).pack()
+Hund1 = tk.Button(app, command=Generator1, activeforeground="green2", fg="green2", activebackground="gray1", bg="gray1", width=100, height=100)
+button1.place(x=-80, y=140)
+Hund1.place(x =-120, y=140)
 # tk.Button
+
+
 
 app.mainloop()
